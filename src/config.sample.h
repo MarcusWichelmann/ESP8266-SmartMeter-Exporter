64de132a2fc27cd669e2c5c0865e4f81bff5d942
Marcus Wichelmann <mail@marcusw.de>
2020-12-27 17:40:53 +0100

    Initial commit

---
+++ src/config.sample.h
@@ -0,0 +1,6 @@
+#pragma once
+
+// Wifi settings
+#define WIFI_SSID "SSID"
+#define WIFI_PASSWORD "PASSWORD"
+
---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+
+void setup()
+{
+  Serial.begin(74880);
+  Serial.println();
+  Serial.println("-- SmartMeter Exporter --");
+}
+
+void loop()
+{
+}
